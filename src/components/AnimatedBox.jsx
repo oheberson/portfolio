b331bcf963dd934db32099394df91cc0faef9a57
heberson <heberson.barros@ibm.com>
2025-03-12 11:40:41 -0300

    spline dots

--- src/components/AnimatedBox.jsx
+++ src/components/AnimatedBox.jsx
@@ -6,12 +6,27 @@ import LowerCornerMenu from "./LowerCornerMenu";
 import LogoContainer from "./LogoContainer";
 import ServicesAndSocial from "./ServicesAndSocial";
 import UpperCornerMenu from "./UpperCornerMenu";
+import Spline from "@splinetool/react-spline";
 
 gsap.registerPlugin(useGSAP);
 
 function AnimatedBox() {
   return (
     <div className="container animated-box">
+      <div
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          borderRadius: "69px",
+          opacity: 0.3,
+        }}
+      >
+        <Spline
+          style={{ borderRadius: 69 }}
+          scene="https://prod.spline.design/f5SzOc8sYqnK7F2W/scene.splinecode"
+        />
+      </div>
       <LogoContainer />
       <UpperCornerMenu />
       <ServicesAndSocial />
